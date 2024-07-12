563a2092f90e970646d264ab4de00dc2223d254e
FizzyApple12 <github@fizzyapple12.com>
2024-07-12 19:01:01 -0400

    Create Initial KiCAD schematics and add a funny phone number to the config

--- Software/src/config.rs
+++ Software/src/config.rs
@@ -5,10 +5,11 @@ pub const DIAL_PULSE_PIN: u8 = 27;
 
 pub const BELL_SOLENOID_PIN: u8 = 23;
 
-pub const KNOWN_NUMBERS: [&str; 5] = [
+pub const KNOWN_NUMBERS: [&str; 6] = [
     "0",                                               // Operator
     "349",                                             // "Fiz"
     "4225",                                            // "Hack"
     "34643664",                                        // "Dingdong",
+    "8675309",                                         // the funny
     "47932786463439686262438634258447455587853896846", // "I swear to god if you manage to dial this ill just let you in"
 ];
